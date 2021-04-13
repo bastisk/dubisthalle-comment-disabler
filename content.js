e580b3a13151f701571cf3a0a4544b82ec2a3be9
bastisk <basti.sk@gmx.de>
2021-04-13 21:32:08 +0200

    removes array index

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 setInterval(() => {
   try {
-    document.getElementById("comments")[0].style.display = "none";
+    document.getElementById("comments").style.display = "none";
   } catch (e) {}
 }, 2000);
